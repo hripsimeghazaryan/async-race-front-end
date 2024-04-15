498906acec29dbd956d131926a187a51facc2aa6
Hripsime Ghazaryan <94200499@mail.ru>
2024-04-15 20:39:30 +0400

    refactor: add return

--- src/pages/Garage.tsx
+++ src/pages/Garage.tsx
@@ -1,4 +1,15 @@
+import RacingLine from "../components/RacingLine";
+import './Garage.css'
+
 function Garage() {
+
+    return (
+        <div className="garage-container">
+            <div className="racing-track-container">
+                <RacingLine />
+            </div>
+        </div>
+    );
 
 }
 
