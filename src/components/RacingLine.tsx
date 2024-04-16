18318ce4a3c7e82dbfb38b5f52117e58e99253dd
Hripsime Ghazaryan <94200499@mail.ru>
2024-04-16 14:03:24 +0400

    fix: separate each racing line

--- src/components/RacingLine.tsx
+++ src/components/RacingLine.tsx
@@ -1,56 +1,46 @@
-import { useState, useEffect } from "react";
-import requests from "../utils/resuests";
+import { useState } from "react";
 import { FaCarSide } from "react-icons/fa6";
 import Button from "./Button";
+import Car from "../interfaces/Car";
+import requests from "../utils/resuests";
 import './RacingLine.css';
-import { title } from "process";
 
-interface Car {
-    name: string,
-    color: string,
-    id: number
-}
-
-function RacingLine() {
-    const [car, setCar] = useState<Car[]>();
+function RacingLine({name, color, id}: Car) {
+    // const [car, setCar] = useState<Car[]>();
     const [isSelected, setIsSelected] = useState<number | null>(null);
+    const [isMoving, setIsMoving] = useState(false);
 
-    useEffect(() => {
-        const get = async () => {
-            const data = await requests.getCars();
-            setCar(data)
-        }
-        get();
-    }, []);
 
     const changeColor = (id: number): void => {
         // changes color of selected car
-        setCar((prev) => prev?.map(elem => (elem.id === id) ? {...elem, color: "purple"} : elem))  
+        // setCar((prev) => prev?.map(elem => (elem.id === id) ? {...elem, color: "purple"} : elem))  
     }
 
     const selectCar = (id: number): void => {
-        setIsSelected(id)
+        // setIsSelected(id)
+    }
+
+    const deleteCar = (id: number): void => {
+        const response = requests.deleteCar(id);
     }
 
     return (
-        <div>{car?.map((item) => (
-            <div key={item.id} className={`line-container ${isSelected === item.id ? 'selected' : ''}`}>
+            <div key={id} className={`line-container ${isSelected === id ? 'selected' : ''}`}>
                 <div className="line line-dir">
                     <div className="side-btn-container">
-                        <Button title="Select" onClick={() => selectCar(item.id)} />
-                        <Button title="Delete" onClick={() => changeColor(item.id)} />
+                        <Button title="Select" onClick={() => selectCar(id)} />
+                        <Button title="Delete" onClick={() => deleteCar(id)} />
                     </div>
                     <div className="side-btn-container race-btn">
-                        <Button title="Start" onClick={() => selectCar(item.id)} />
-                        <Button title="Stop" onClick={() => changeColor(item.id)} />
+                        <Button title="Start" onClick={() => selectCar(id)} />
+                        <Button title="Stop" onClick={() => changeColor(id)} />
                     </div>
                     <div className="car">
-                        <FaCarSide fontSize="2em" color={item.color}/>
+                        <FaCarSide fontSize="2em" color={color}/>
                     </div>
-                    <span className="car-name">{item.name}</span>
+                    <span className="car-name">{name}</span>
                 </div>
             </div>
-        ))}</div>
     )
 
 } 
