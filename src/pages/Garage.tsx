34fc7df4cd44af744cffe823879d48372b65e377
Hripsime Ghazaryan <94200499@mail.ru>
2024-04-16 14:02:45 +0400

    feat: add createCar function

--- src/pages/Garage.tsx
+++ src/pages/Garage.tsx
@@ -1,12 +1,36 @@
-import RacingLine from "../components/RacingLine";
+import { useState } from "react";
+import RacingField from "../components/RacingField";
+import requests from "../utils/resuests";
+import Car from "../interfaces/Car";
+import CreateCar from "../interfaces/CreateCar";
 import './Garage.css'
 
 function Garage() {
+    const [newName, setNewName] = useState<string>("");
+    const [newColor, setNewColor] = useState<string>("");
+
+    const createCar = async () => {
+        const carData: CreateCar = {
+            name: newName,
+            color: newColor
+        }
+        const response = await requests.createCar(carData);
+    }
 
     return (
         <div className="garage-container">
+            <div className="garage-header">
+                <h1 className="page-title garage-title">Garage</h1>
+            </div>
+            <div className="create-car-container">
+                <form onSubmit={createCar} className="create-car-form">
+                    <input type="text" value={newName} onChange={(event) => setNewName(event?.target.value)}></input>
+                    <input type="color" value={newColor} onChange={(event) => setNewColor(event?.target.value)}></input>
+                    <input type="submit"></input>
+                </form>
+            </div>
             <div className="racing-track-container">
-                <RacingLine />
+                <RacingField />
             </div>
         </div>
     );
