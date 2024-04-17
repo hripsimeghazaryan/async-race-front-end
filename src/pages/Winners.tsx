3f0b1eb1f044940b205dbc729b699dc4451d9bdf
Hripsime Ghazaryan <94200499@mail.ru>
2024-04-18 00:21:40 +0400

    refactor: fill winners component

--- src/pages/Winners.tsx
+++ src/pages/Winners.tsx
@@ -1,5 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
 function Winners() {
-
+  return (
+    <div className="page-container">
+      <div className="winners-header">
+        <h1 className="page-title winners-title">Winners</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Winners;
