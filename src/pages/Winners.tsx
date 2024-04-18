6129b1941e1a66d908e31c53c8927d7de5fecba2
Hripsime Ghazaryan <94200499@mail.ru>
2024-04-18 23:41:58 +0400

    feat: fill winner component

--- src/pages/Winners.tsx
+++ src/pages/Winners.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
 function Winners() {
@@ -5,6 +7,21 @@ function Winners() {
     <div className="page-container">
       <div className="winners-header">
         <h1 className="page-title winners-title">Winners</h1>
+      </div>
+      <div className="winners-content">
+        <div className="winners-table">
+          <table>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Car</th>
+                <th>Name</th>
+                <th>Wins</th>
+                <th>Best Time (Seconds)</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
       </div>
     </div>
   );
