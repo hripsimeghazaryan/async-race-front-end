74821677587614bbf88385cfaac13dbd1a5dae37
Hripsime Ghazaryan <94200499@mail.ru>
2024-04-20 21:38:00 +0400

    feat: add engine state and functions (not done)

--- src/components/RacingLine.tsx
+++ src/components/RacingLine.tsx
@@ -1,8 +1,8 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useRef } from 'react';
 import { FaCarSide } from 'react-icons/fa6';
 import Button from './Button';
 import Car from '../interfaces/Car';
-import Engine from '../interfaces/Engine';
+import requests from '../utils/requests';
 import { GarageType } from '../interfaces/GarageType';
 import { GarageDataContext } from '../contexts/garage-data';
 import animate from 'animejs';
@@ -15,38 +15,50 @@ type Props = {
 }
 
 function RacingLine({ car, select, onSelect }: Props) {
-  const [engineStatus, setEngineStatus] = useState<'started' | 'stopped' | 'drive'>('stopped');
-  const [carPosition, setCarPosition] = useState<number>(0);
+  const engineStatus = useRef<'started' | 'stopped' | 'drive'>('stopped');
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const carPosition = useRef<number>(0);
+  // const [carPosition, setCarPosition] = useState<number>(0);
   const { deleteCar } = useContext(GarageDataContext) as GarageType;
 
-  const handleEngine = async (id: number, status: 'started' | 'stopped') => {
-    setEngineStatus('started');
-    const response = await fetch(`http://127.0.0.1:3000/engine?id=${id}&status=${status}`, {
-      method: 'PATCH',
-    });
-    const { velocity, distance } = await response.json();
-
-    if(response.ok) {
+  const handleStartEngine = async () => {
+    if(engineStatus.current === 'stopped') {
+      engineStatus.current = 'started';
+      const response = await requests.startStopEngine(car.id, 'started');
+      const { distance, velocity } = response;
       const duration = (distance / velocity) / 1000;
-      setEngineStatus('drive');
+      engineStatus.current = 'drive';
+      const driveRes = await requests.driveEngine(car.id);
+    }
+  }
 
-      const newresponse = await fetch(`http://127.0.0.1:3000/engine?id=${id}&status=drive`, {
-        method: 'PATCH',
-      });
-      const car = document.getElementById(`${id}`);
-      car?.style.setProperty('--velocity', `${duration}`);
-      car?.style.setProperty('--car-position', `${carPosition}`);
+  const handleStopEngine = async () => {
+    if(engineStatus.current === 'drive') {
+      engineStatus.current = 'stopped';
+      const response = await requests.startStopEngine(car.id, 'stopped');
+    }
+  }
 
-      // animate({
-      //   targets: car,
-      //   translateX: [
-      //     '0px',
-      //     ''
-      //   ],
-      //   duration: duration,
-      //   easing: 'easeInOutSine',
-      //   loop: false,
-      // });
+  const handleEngine = async (status: 'started' | 'stopped') => {
+    if(engineStatus.current === 'stopped') {
+      engineStatus.current = 'started';
+      const response = await requests.startStopEngine(car.id, status);
+      const { distance, velocity } = response;
+      engineStatus.current = 'drive';
+      const driveResp = await requests.driveEngine(car.id);
+      const duration = (distance / velocity) / 1000;
+      const parentContainer = document.querySelector('.racing-road')?.clientWidth || 0;
+
+      const updatePosition = (speed: number) => {
+        if(carPosition.current < parentContainer) {
+          carPosition.current += speed;
+        } else {
+          clearInterval(intervalRef.current!);
+          engineStatus.current = 'stopped';
+        }
+      };
+      setInterval(updatePosition, 100) as unknown as NodeJS.Timeout;
+      console.log(carPosition.current)
     }
   }
 
@@ -58,17 +70,38 @@ function RacingLine({ car, select, onSelect }: Props) {
     <div key={car.id} className={`line-container ${select ? 'selected' : ''}`}>
       <div className="line line-dir">
         <div className="side-btn-container">
-          <Button title="Select" onClick={() => onSelect(car.id)} />
-          <Button title="Delete" onClick={() => handleDelete(car.id)} />
+          <Button
+          title="Select"
+          onClick={() => onSelect(car.id)}
+          />
+          <Button
+          title="Delete"
+          onClick={() => handleDelete(car.id)}
+          />
         </div>
         <div className="side-btn-container race-btn">
-          <Button title="Start" onClick={() => handleEngine(car.id, 'started')} />
-          <Button title="Stop" onClick={() => handleEngine(car.id, 'stopped')} />
+          <Button
+          title="Start"
+          onClick={() => handleEngine('started')}
+          />
+          <Button
+          title="Stop"
+          onClick={() => handleEngine('stopped')}
+          />
         </div>
-        <div id={`${car.id}`} className={`car ${engineStatus === 'drive' ? 'car-animation' : ''}`} style={{ transform: `translateX(${carPosition}px)` }}>
-          <FaCarSide fontSize="2em" color={car.color} />
+        <div className="racing-road">
+          <div
+          id={`${car.id}`}
+          className={`car ${engineStatus.current === 'drive' ? 'car-animation' : ''}`}
+          style={{
+            transform: `translateX(${carPosition.current}px)`,
+
+          }}
+          >
+            <FaCarSide fontSize="2em" color={car.color} />
+          </div>
+          <span className="car-name">{car.name}</span>
         </div>
-        <span className="car-name">{car.name}</span>
       </div>
     </div>
   );
