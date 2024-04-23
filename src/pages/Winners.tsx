70d45d82739dd8474ec1e9d60f160b70183f9526
Hripsime Ghazaryan <94200499@mail.ru>
2024-04-24 00:24:24 +0400

    feat: add sorting to winners component

--- src/pages/Winners.tsx
+++ src/pages/Winners.tsx
@@ -1,16 +1,19 @@
 import { Col, Row } from "antd";
-import { FaCarSide } from 'react-icons/fa6';
 import Header from "../components/Header";
 import PaginationComp from "../components/Pagination";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { WinnersType } from "../interfaces/WinnersType";
 import { WinnersDataContext } from "../contexts/winners-data";
 import WinnerField from "../components/WinnerField";
+import usePersistency from "../hooks/Persistency";
 import './Winners.css';
 
 
 function Winners() {
-  const { pagination, changePagination } = useContext(WinnersDataContext) as WinnersType;
+  const [idOrder, setIdOrder] = useState<'ASC' | 'DESC'>('DESC');
+  const [winsOrder, setWinsOrder] = useState<'ASC' | 'DESC'>('DESC');
+  const [timeOrder, setTimeOrder] = useState<'ASC' | 'DESC'>('DESC');
+  const { pagination, changePagination, sortWinners } = useContext(WinnersDataContext) as WinnersType;
 
   const handleNextPage = (page: number, limit: number) => {
     changePagination(page + 1, limit);
@@ -20,16 +23,37 @@ function Winners() {
     changePagination(page - 1, limit);
   };
 
+  const handleIdOrder = () => {
+    sortWinners('id', idOrder);
+    setIdOrder(idOrder === 'ASC' ? 'DESC' : 'ASC')
+  }
+
+  const handleWinsOrder = () => {
+    sortWinners('wins', idOrder);
+    setIdOrder(idOrder === 'ASC' ? 'DESC' : 'ASC')
+  }
+
+  const handleTimeOrder = () => {
+    sortWinners('time', idOrder);
+    setIdOrder(idOrder === 'ASC' ? 'DESC' : 'ASC')
+  }
+
   return (
     <div className="page-container">
       <Header title={"Winner"} total={pagination.total} />
       <div className="winners-table">
         <Row className="table-header table-row">
-          <Col span={4} order={1}>No</Col>
-          <Col span={4} order={2}>Car</Col>
-          <Col span={4} order={3}>Name</Col>
-          <Col span={4} order={4}>Wins</Col>
-          <Col span={4} order={5}>Best Time (Seconds)</Col>
+          <Col className="column" span={4} order={1}>
+            <div className="column-title" onClick={handleIdOrder}>№</div>
+          </Col>
+          <Col className="column" span={4} order={2}>Car</Col>
+          <Col className="column" span={4} order={3}>Name</Col>
+          <Col className="column" span={4} order={4}>
+            <div className="column-header" onClick={handleWinsOrder}>Wins</div>
+          </Col>
+          <Col className="column" span={4} order={5}>
+            <div className="column-header" onClick={handleTimeOrder}>Best Time</div>
+          </Col>
         </Row>
         <WinnerField />
         <PaginationComp
