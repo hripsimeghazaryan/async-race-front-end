cf4c6832b2c8101a24d4d44c058c84e9469fadc1
Hripsime Ghazaryan <94200499@mail.ru>
2024-05-17 00:29:46 +0400

    feat: add defineplugin for dotenv

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,16 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
 import * as webpack from 'webpack';
 import 'webpack-dev-server';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import dotenv from 'dotenv';
+
+const env = dotenv.config().parsed;
+const envKeys = env ? Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {} as { [key: string]: string }) : {};
 
 const config: webpack.Configuration = {
   mode: 'development',
@@ -36,14 +45,15 @@ const config: webpack.Configuration = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
+    new webpack.DefinePlugin(envKeys),
   ],
   devServer: {
     historyApiFallback: true,
     static: {
       directory: path.join(__dirname, 'public'),
     },
-    port: 8080
-  }
+    port: 8080,
+  },
 };
 
 export default config;
